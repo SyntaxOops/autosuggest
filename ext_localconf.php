ed63441e7413a08016ac91aaf56d5319a476ab5e
d-haythem <haythemdaoud.x@gmail.com>
2025-09-14 03:11:42 +0100

    [TASK] Clean up unwanted code

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,12 +4,26 @@ defined('TYPO3') or die;
 
 use SyntaxOOps\Autosuggest\Service\NewsSuggestService;
 use SyntaxOOps\Autosuggest\Xclass\Form\Element\InputTextElement;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['autosuggest'][] = 'SyntaxOOps\\Autosuggest\\ViewHelpers';
 
 // news autosuggest suggestion service
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['autosuggest']['news'] = NewsSuggestService::class;
 
+// Extend backend input field
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Form\Element\InputTextElement::class] = [
     'className' => InputTextElement::class,
 ];
+
+// Add JS and CSS
+ExtensionManagementUtility::addTypoScriptSetup('
+    page {
+        includeJSFooter {
+            jsAutosuggest = EXT:autosuggest/Resources/Public/Js/autosuggest.js
+        }
+        includeCSS {
+            cssAutosuggest = EXT:autosuggest/Resources/Public/Css/autosuggest-bootstrap.css
+        }
+    }
+');
